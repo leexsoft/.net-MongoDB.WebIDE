1784c1d7f4da651349430add63d237b24a4be532
lxs <leexsoft@163.com>
2014-09-23 17:54:13 +0800

    增加索引管理的前台页面部分

---
+++ MongoDB.WebIDE/Content/js/showindex.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#btnAddColumn').click(function () {
+        var field = $('#txtField').val();
+        var orderText = $('#selOrder option:selected').text();
+        var orderVal = $('#selOrder option:selected').val();
+
+        if (field != '') {
+            $('#tblColumn').append('<tr><td>' + field + '</td><td value=' + orderVal + '>' + orderText + '</td><td><a action="del">删除</a></td></tr>');
+        } else {
+            alert('请输入要添加的列!');
+        }
+    })
+});
